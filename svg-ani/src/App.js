b87cfe12fdc1c1525efc5c1137a405cd4673444e
andy <symbol_for_success@hotmail.com>
2019-02-22 10:43:11 +1300

    responsive done

---
+++ svg-ani/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SvgGarden from './components/svg-garden/svg-garden'
+class App extends Component {
+render() {
+    return (
+        <div>
+          <SvgGarden/>
+
+        </div>
+    );
+  }
+}
+export default App;
